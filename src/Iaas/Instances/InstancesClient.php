a0a4b2d4da829167b3f0c481221e007767e28ddb
wudi <wudi@51idc.com>
2017-03-16 13:35:50 +0800

    Add comments

--- src/Iaas/Instances/InstancesClient.php
+++ src/Iaas/Instances/InstancesClient.php
@@ -18,11 +18,25 @@ use Anchcloud\Iaas\IaasClient;
 class InstancesClient extends IaasClient
 {
 
+    /**
+     * 开通主机实例
+     *
+     * @param $zone
+     * @param array $params
+     * @return bool|mixed|string
+     */
     public function runInstances($zone, array $params = [])
     {
         return $this->send('POST', "/v2/zone/{$zone}/instances", ['json' => $params]);
     }
 
+    /**
+     * 查询主机实例列表
+     *
+     * @param $zone
+     * @param array $params
+     * @return bool|mixed|string
+     */
     public function describeInstances($zone, array $params = [])
     {
         return $this->send('GET', "/v2/zone/{$zone}/instances", ['query' => $params]);
